f0520bc1f8b5fa38c1d7e76b1c74c9e6f6b2fb32
Felix Wang <felixw17@stanford.edu>
2020-01-05 21:32:48 -0800

    server: testing basic server calls

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          This is a test by Felix!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    test: ""
+  };
+
+  constructor() {
+    super();
+  }
+
+  init() {
+    fetch(`/sum/3/5`)
+      .then(res => res.text())
+      .then(res => {
+        this.setState({ test: res });
+      });
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>This is a test by Felix: {this.state.test}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
