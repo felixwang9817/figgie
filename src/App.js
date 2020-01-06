6dde8a97af15b4bd3dc4bae734e4d4ffcdb75802
Felix Wang <felixw17@stanford.edu>
2020-01-05 21:10:51 -0800

    server: added Express server

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This is a test by Felix!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
