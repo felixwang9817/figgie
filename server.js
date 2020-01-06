44321486954bba107e316f52418c86d24606d5c1
Felix Wang <felixw17@stanford.edu>
2020-01-06 14:56:04 -0800

    server: factored out parsing function for handling client commands and introduced several server calls to handle basic market clearing after a trade

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(express.static(__dirname));
 http.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 var suits = ["hearts", "diamonds", "clubs", "spades"];
-
+var actions = ["take", "sell"];
 
 // state
-state = {
+var initialMarketState = {
   bids: {
     clubs: { bid: null, player: null },
     spades: { bid: null, player: null },
@@ -26,40 +26,81 @@ state = {
   }
 };
 
+var marketState = deepCopy(initialMarketState);
+
+function parseCommand(command) {
+  console.log("command: " + command);
+  let tokens = command.split(" ");
+
+  if (tokens.length == 2) {
+    // take or sell command
+    let action = tokens[0];
+    let suit = tokens[1];
+    if (!actions.includes(action) || !suits.includes(suit)) {
+      return false;
+    }
+
+    console.log("take or sell command detected");
+    if (action == "take") {
+      takeOffer(suit);
+    }
+  }
+  if (tokens.length == 3) {
+    // offer command
+    let suit = tokens[0];
+    let price = Number(tokens[2]);
+    if (!suits.includes(suit) || tokens[1] != "at" || isNaN(price)) {
+      return false;
+    }
+
+    console.log("offer command detected");
+    postOffer(suit, price);
+  }
+}
+
+function takeOffer(suit) {
+  let currentOffer = marketState["offers"][suit]["offer"];
+  let seller = marketState["offers"][suit]["player"];
+
+  // execute trade on market and player data
+  // TODO: move player data from App.js to server.js and handle player execution later
+
+  // TODO: clear market at end
+  clearMarket();
+}
+
+function clearMarket() {
+  marketState = deepCopy(initialMarketState);
+  console.log("clearMarket: " + JSON.stringify(marketState));
+  io.emit("market_update", marketState);
+}
+
+function postOffer(suit, price, player = 0) {
+  let currentOffer = marketState["offers"][suit]["offer"];
+  console.log("currentOffer: " + currentOffer);
+  console.log("price: " + price);
+  if (price > currentOffer) {
+    marketState["offers"][suit]["offer"] = price;
+    marketState["offers"][suit]["player"] = player;
+    io.emit("market_update", marketState);
+    console.log("final marketState after postOffer: " + marketState);
+  }
+}
+
+function deepCopy(x) {
+  return JSON.parse(JSON.stringify(x));
+}
+
 io.on("connection", function(socket) {
   // on connection, server determines unique id for the socket and stores in a dictionary
   console.log("a user connected");
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
-  
+
   // wait for client action
-  socket.on("client_command", msg => {
-    console.log("server has received command: " + msg);
-
-    // parse
-    let tokens = msg.split(" ");
-    console.log(tokens);
-    if (tokens.length == 3) {
-      // offer command
-      let suit = tokens[0];
-      let price = Number(tokens[2]);
-      if (!suits.includes(tokens[0]) || tokens[1] != "at" || isNaN(tokens[2])) {
-        socket.emit("bad_command");
-        return false;
-      }
-      // edit state here, then send new state to client for update
-      // TODO: add verification such as the new bid is better than existing bid
-      state["offers"][suit]["offer"] = price;
-      // TODO: add player after settling unique id for socket
-      // io.emit("market_update", JSON.stringify(state));
-      console.log(state);
-      io.emit("market_update", state);
-    } else {
-      // TODO: bid command
-      return false;
-    }
-
-    // TODO: player states
+  socket.on("client_command", command => {
+    console.log("server has received command: " + command);
+    parseCommand(command);
   });
 });
