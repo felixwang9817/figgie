6a5658c8452660c10036d07aa293e6d8178c617a
Wanqi Zhu <1213.ghs@gmail.com>
2020-01-05 21:37:35 -0800

    frontadd mock

--- src/App.js
+++ src/App.js
@@ -2,22 +2,118 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      diamonds: 2,
+      clubs: null,
+      hearts: null,
+      spades: null,
+      num_cards: 10,
+      money: 50,
+      id: props.id,
+    };
+  }
+
+
+  render() {
+    var cards = '';
+
+    // TODO: prettify this
+    if (this.state.diamonds === null) {
+      cards += '';
+    } else {
+      cards += this.state.diamonds.toString() + ' diamonds '
+    }
+
+    if (this.state.clubs === null) {
+      cards += '';
+    } else {
+      cards += this.state.clubs.toString() + ' clubs '
+    }
+
+    if (this.state.hearts === null) {
+      cards += '';
+    } else {
+      cards += this.state.hearts.toString() + ' hearts '
+    }
+
+    if (this.state.spades === null) {
+      cards += '';
+    } else {
+      cards += this.state.spades.toString() + ' spades '
+    }
+
+
+    return (
+      <div>
+        <span class='player_id'> player #{this.state.id} </span>
+        {cards}
+        <span class='num_cards'> {this.state.num_cards} cards </span>
+        <span class='money'> {this.state.money} money </span>
+      </div>
+    );
+  }
+}
+
+
+class Market extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bids: {
+        clubs: [1, 0],
+        spades: [2, 0],
+        hearts: [3, 2],
+        diamonds: [4, 1],
+      },
+      offers: {
+        clubs: 11,
+        spades: 12,
+        hearts: 13,
+        diamonds: 14,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div id='market'>
+        market
+
+        {
+          Object.keys(this.state.bids).map((key, val) => ( 
+
+            <p> {this.state.bids[key][0]} bid (#{this.state.bids[key][1]})
+             for {key}. </p> 
+          ))
+        }
+
+        {// TODO: offers, display in bidding language?
+        }
+
+      </div>
+    );
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div id='players'>
+        <Player id="0" />
+        <Player id="1" />
+        <Player id="2" />
+        <Player id="3" />
+      </div>
+
+      <Market />
+
       </header>
     </div>
   );
