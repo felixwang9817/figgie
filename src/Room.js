bdb1d7a6a31b51f876f2878749e7bf62bbe4618a
Wanqi Zhu <1213.ghs@gmail.com>
2020-02-08 20:25:27 -0800

    try multiple pages

---
+++ src/Room.js
@@ -0,0 +1,21 @@
+import React from "react";
+import queryString from "query-string";
+
+class Room extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { roomNumber: "" };
+
+  }
+
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    );
+  }
+}
+
+export default Room;
